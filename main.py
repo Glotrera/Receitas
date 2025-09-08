4a76a6ef800ce4b8f7bf9481aaf7094517b7c3d0
FranciscoC <franciscolcpereira99@gmail.com>
2025-09-08 11:43:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from datetime import datetime
 from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
@@ -53,3 +53,42 @@ class ingredientes_receita(db.Model):
     ingrediente_id = db.Column(db.Integer, db.ForeignKey('ingrediente.id'), nullable=False)
     quantidade = db.Column(db.String(50), nullable=False)
     unidade = db.Column(db.String(50), nullable=False)
+    
+@app.route ('/regiter', methods=['GET', 'POST'])
+def register():
+    
+    if current_user.is_authenticated:
+        return redirect(url_for('dashboard'))
+    
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        
+        hashed_password = generate_password_hash(password, method='utf-8')
+        
+        new_user = User(username=username, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Conta criada com sucesso! Faça login.', 'success')
+        return redirect(url_for('login'))
+    
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('dashboard'))
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('dashboard'))
+        else:
+            flash('Login falhou. Verifique seu nome de usuário e senha.')
+            
+    return render_template('login.html')
+
+
